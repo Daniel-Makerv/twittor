84747600d4dce88a64e2bf8f3f2f1995468deb69
Jose <Danieldlcr5@gmail.com>
2019-12-06 03:43:22 -0600

    modificaciones del iphone x+

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    // '/',
+    //'/'
     , 'index.html'
     , 'css/style.css'
     , 'img/favicon.ico'
@@ -34,11 +34,11 @@ en la etapa de instalación del SW
 */
 self.addEventListener('install', e => {
     const cacheStatic = caches.open(STATIC_CACHE)
-        .then(cache => cache.addAll(APP_SHELL))
+        .then(cache => cache.addAll(APP_SHELL));
     const cacheInmutable = caches.open(INMUTABLE_CACHE)
-        .then(cache => cache.addAll(APP_SHELL_INMUTABLE))
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]))
-})
+        .then(cache => cache.addAll(APP_SHELL_INMUTABLE));
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]) )
+});
 
 /*
 borramos cachés estáticos que pueden ya no estar vigentes
@@ -49,23 +49,26 @@ self.addEventListener('activate', e => {
         keys.forEach( key => {
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
+              }
+              if ( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                          return caches.delete(key);
             }
         });
     });
     e.waitUntil( respuesta );
-})
+});
 
-self.addEventListener('activate', e => {
-    const respuesta = caches.keys().then( keys => {
-        keys.forEach( key => {
-
-            if (key !== STATIC:CACHE && key.includes('static') ) {
-                return caches.delete(key);;
-            }
-              if ( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
-                        return caches.delete(key);
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match(e.request)
+        .then(res => {
+            if (res) {
+                return res;
+            } else {
+                return fetch(e.request)
+                    .then(newRes => {
+                        return actualizaCache(DYNAMIC_CACHE, e.request, newRes);
                     })
             }
         });
-    e.waitUntil(respuesta );
-});
+    e.respondWith(respuesta);
+})
